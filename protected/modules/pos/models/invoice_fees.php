193e61738fbbaff0cd894399173518bf3f3c9700
Farid Efendi <miftahfaridse@gmail.com>
2019-11-13 12:13:37 +0700

    Calculate fee also from refund

--- protected/modules/pos/models/invoice_fees.php
+++ protected/modules/pos/models/invoice_fees.php
@@ -33,7 +33,7 @@ class InvoiceFeesModel extends \Model\BaseModel
     public function getData($data = array())
     {
         $sql = 'SELECT t.*, i.serie AS invoice_serie, i.nr AS invoice_nr, i.status AS invoice_status, i.config AS invoice_configs, i.paid_at, i.delivered_at,
-            w.title AS warehouse_name, (SELECT SUM(ii.price*ii.quantity) FROM {tablePrefix}ext_invoice_item ii WHERE ii.invoice_id = t.invoice_id) AS total_revenue, SUM(t.fee) AS total_fee, COUNT(t.id) AS total_transaction     
+            w.title AS warehouse_name, (SELECT SUM(ii.price*ii.quantity) FROM {tablePrefix}ext_invoice_item ii WHERE ii.invoice_id = t.invoice_id) AS total_revenue, SUM(t.fee + t.fee_refund) AS total_fee, COUNT(t.id) AS total_transaction     
             FROM {tablePrefix}ext_invoice_fee t 
             LEFT JOIN {tablePrefix}ext_warehouse w ON w.id = t.warehouse_id 
             LEFT JOIN {tablePrefix}ext_invoice i ON i.id = t.invoice_id 
@@ -86,7 +86,7 @@ class InvoiceFeesModel extends \Model\BaseModel
 	public function getSummaryData1($data = array())
     {
         $sql = 'SELECT DATE_FORMAT(t.created_at, "%Y-%m-%d") AS created_date, COUNT(t.invoice_id) AS total_transaction, 
-			SUM(ii.price*ii.quantity) AS total_revenue, SUM(t.fee) AS total_fee     
+			SUM(ii.price*ii.quantity) AS total_revenue, SUM(t.fee + t.fee_refund) AS total_fee     
             FROM {tablePrefix}ext_invoice_fee t 
             LEFT JOIN {tablePrefix}ext_warehouse w ON w.id = t.warehouse_id 
             LEFT JOIN {tablePrefix}ext_invoice i ON i.id = t.invoice_id 
@@ -159,7 +159,7 @@ class InvoiceFeesModel extends \Model\BaseModel
 		$sql = 'SELECT cuk.created_date, SUM(cuk.total_revenue1) AS total_revenue, COUNT(cuk.created_date) AS total_transaction, SUM(cuk.total_fee1) AS total_fee, cuk.invoice_configs1 AS invoice_configs   
 			FROM (SELECT DATE_FORMAT(t.created_at, "%Y-%m-%d") AS created_date, 
 				(SELECT SUM(ii.price*ii.quantity) FROM {tablePrefix}ext_invoice_item ii WHERE ii.invoice_id = t.invoice_id) AS total_revenue1, 
-				SUM(t.fee) AS total_fee1, i.config AS invoice_configs1 FROM {tablePrefix}ext_invoice_fee t 
+				SUM(t.fee + t.fee_refund) AS total_fee1, i.config AS invoice_configs1 FROM {tablePrefix}ext_invoice_fee t 
 				LEFT JOIN {tablePrefix}ext_invoice i ON i.id = t.invoice_id
             	WHERE 1 '. $where .'  
 				GROUP BY t.invoice_id ORDER BY t.created_at ASC) AS cuk 
